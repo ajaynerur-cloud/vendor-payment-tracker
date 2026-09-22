e6874c295129f8e3874d74b4a1385ac3f50186f7
ajaynerur-cloud <ajaynerur@gmail.com>
2026-09-22 23:34:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PAYMENT_APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT_REF.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://baioxfbpmprvhhyxwjtx.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_UKMVdpGLlOvHD1Y76H7a3w_cJIXsILo"
 };
